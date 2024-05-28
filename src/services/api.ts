3907155da6e682861e5bda8aab008f99924eba81
artmusicrr <artmusic_rr@msn.com>
2024-05-27 22:44:57 -0300

    atualização do end point

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,9 @@ export const updateText = async (query: any, id: any, content: any, ) => {
   console.log("Conteúdo:", content)
   console.log("Query:", query)
     
-      const response = await fetch(`http://localhost:4000/title/${query}/${id}`, {
+      //const response = await fetch(`http://localhost:4000/title/${query}/${id}`, {
+        
+      const response = await fetch(`https://master-back-deploy.onrender.com/${query}/${id}`, {
         method: 'PATCH',
         headers: {
           'Content-Type': 'application/json',
